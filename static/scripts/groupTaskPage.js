4d8841c49304c7d07bec218631743d631d2a41d9
mge19 <34735066+mge19@users.noreply.github.com>
2021-02-07 21:28:34 +0300

    Update groupTaskPage.js

--- static/scripts/groupTaskPage.js
+++ static/scripts/groupTaskPage.js
@@ -7,12 +7,26 @@ function setMinDate(){
     {
 	x[i].setAttribute("min",date);}
 }
-function show_hide(x){
-  var z = document.getElementsByClassName(x);
-  if (z[0].style.display == "none") {
-    z[0].style.display = "block";} 
+function show_hide(a,b){
+  let id=$(b).attr('id');
+  let pos=$('[id="'+id+'"]').index($(b));
+  console.log(pos);
+  var x = $('.'+a)[pos];
+  if (x.style.display == "none") {
+    x.style.display = "block";
+    if(id=="show"){
+      b.innerHTML="Hide";}
+    if(id=="update"){
+      setMinDate();
+      b.innerHTML="Don't Update";}
+  } 
   else {
-    z[0].style.display = "none";}	
+    x.style.display = "none";
+    if(id=="show"){
+      b.innerHTML="Show";}
+    if(id=="update"){
+      b.innerHTML="Update";}
+  } 
 }
 function convert_date(year,month,day){
     var new_month=month;
@@ -70,10 +84,11 @@ $(document).ready(function () {
                     month = task["month"];
                     year = task["year"];
                     inhtml += '<ul class="sets">';
-                    inhtml += '<li class="head" onclick=show_hide("show'+i+'")>' + day + '-' + month + '-' + year + '</li>';
-                    inhtml += '<li class="show'+i+'" style="display:none">' + task["title"] + '<br><br>' + task["urgency"];
-		    inhtml += '<br><br>' + task["message"]+'</li></ul>';
+                    inhtml += '<li id="head" onclick=show_hide("show_task",this)>' + day + '-' + month + '-' + year + '</li>';
+                    inhtml += '<li class="show_task" style="display:none">' + task["title"] + '<br><br>' + task["urgency"];
+		    inhtml += '<br><br>' + task["message"]+'</li>';
             }
+            inhtml += '</ul>';
             console.log(inhtml);
             table.append(inhtml);
         },
@@ -82,16 +97,35 @@ $(document).ready(function () {
 
 //input fieldlarin degerlerini doldurmak
 $(document).ready(function () {
-    for (var i = 1 i <= 31; i++) {
-        snippet = `<option value="${i}">${i}</option>`
-        $('.days').append(snippet); 
+    //fill days
+    for (var i = 1; i <= 31; i++) {
+       snippet = '<option value='+i+'>'+i+'</option>'
+       $('.days').append(snippet);
     }
+    let month=$('.ymddiv > .months');
+    $(month).on('change',function(e){
+        let days=$('.days');
+        e.preventDefault();
+        let limit=31;
+        let val=month.val();
+        if(val===months[1]){
+              limit=29;
+        }
+        else if(val===months[3] || val===months[5] || val===months[8] || val===months[10]){
+           limit=30;
+        }
+        days.empty();        
+        for (var i = 1; i <= limit; i++) {
+           snippet = '<option value='+i+'>'+i+'</option>'
+           days.append(snippet);
+        }
+    });
     for (var i = 0; i < months.length; i++) {
-        snippet = `<option value="${months[i]}">${months[i]}</option>`
+        snippet = '<option value='+months[i]+'>'+months[i]+'</option>'
         $('.months').append(snippet); 
     }
     for(var i = 0; i < 3; i++) {
-	snippet = `<option value="${urgency[i]}">${urgency[i]}</option>`
+	snippet = '<option value='+urgency[i]+'>'+urgency[i]+'</option>'
         $('.urgency').append(snippet); 
     }
 });
@@ -160,12 +194,12 @@ $(document).ready(function () {
                 for (var i = 0; i < searchTable["items"].length; i++) {
                     var task = searchTable["items"][i];
                     inhtml += "<tr><td>" + task["day"] + "." + task["month"] + "." + task["year"] + "</td><td>" + task["time"] + "</td>   <td>" + task["title"] + "</td><td>" + task["urgency"] + "</td>"
-                    inhtml += '<td><a href="#" class="btn btn-primary btn-sm show">Show</a></td>'
+                    inhtml += '<td><a href="#" onclick=show_hide("showdesc",this) id="show" class="btn btn-primary btn-sm">Show</a></td>'
                     inhtml += '<td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>';
-		    inhtml += '<td><a href="#" class="btn btn-success btn-sm update">Update</a></td></tr>';
-		    inhtml += '<tr style="display:none" id=' + i + '><td>' + task["message"] + '</td></tr>';                
-		    inhtml += '<tr style="display:none" id="update' + i + '"><td><strong>Update a task</strong><br><br>';
-		    inhtml += '<form method="post"><label for="date">New Date</label><input type="date" name="date" value="'+convert_date(String(task["year"]),String(task["month"]),String(task["day"]))+'"><br><br>';
+		    inhtml += '<td><a href="#" onclick=show_hide("updateForm",this) id="update" class="btn btn-success btn-sm">Update</a></td></tr>';
+		    inhtml += '<tr style="display:none" class="showdesc"><td>' + task["message"] + '</td></tr>';                
+		    inhtml += '<tr style="display:none" class="updateForm"><td><strong>Update a task</strong><br><br>';
+		    inhtml += '<form class="update_form" method="post"><label for="date">New Date</label><input type="date" name="date" value="'+convert_date(String(task["year"]),String(task["month"]),String(task["day"]))+'"><br><br>';
 		    inhtml += '<label for="time">New Time</label><input type="time" name="time" value="'+convert_time(task["time"])+'"><br><br>';
 		    inhtml += '<label for="urgency">New Urgency</label><select name="urgency" class="urgency">';
 		    for(var j=0 ; j < 3 ; j++) {
@@ -176,7 +210,7 @@ $(document).ready(function () {
 		    }
 		    inhtml += '</select><br><label for="title">New Title</label><input type="text" name="title" placeholder="New Title" value="' + task["title"] + '"><br><br>';
 		    inhtml += '<label for="message">New Message</label><textarea name="message" placeholder="New Message">' + task["message"] + '</textarea><br><br>';	
-		    inhtml += '<input type="submit" id="updateButton'+i+'" value="Update" class="btn btn-success btn-block add-btn"></form></td></tr>'
+		    inhtml += '<input type="submit" value="Update" class="btn btn-success btn-block add-btn"></form></td></tr>'
 		}
                 table.append(inhtml);
             },
@@ -184,77 +218,48 @@ $(document).ready(function () {
     });
 });
 
-//Tabloda ki show, delete butonlari fonksiyonlari
+//Tabloda ki show, delete,update butonlari fonksiyonlari
 $(document).ready(function () {
     var $parent = $("#book-list");
     //show button
-    $parent.on('click', '.show', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-	let pos = $('.show').index($(this));
-        console.log(pos);
-        let desc = $('#' + pos);
-	if($(this).html()=="Show"){
-		desc.css('display','block');
-		$(this).html('Hide');}
-	else{
-		desc.css('display','none');
-		$(this).html('Show');}
-	
-    });
     $parent.on('click', '.delete', function (e) {
         e.preventDefault();
         let pos = $('.delete').index($(this));
         let taskid = searchTable["items"][pos]["alert_id"];
-        $(this).closest("tr").remove();
         searchTable["items"].splice(pos, 1);
         console.log(taskid);
         //send query to remove id
 
         $.ajax({
             type: "POST",
-            url: "/deleteGroupAlert",
+            url: "/deleteUserAlert",
             data: {
                 value: taskid
             },
             success: function (data) {
-                console.log("AJAX RETURN: ", data);
+                window.location.reload();
             },
         });     
+    });   
+    $parent.on('submit', '.update_form', function (e2) {
+	e2.preventDefault();
+	var formData=new FormData();
+	let pos = $('.update_form').index($(this));
+        let taskid = searchTable["items"][pos]["alert_id"];
+	formData.append('id',taskid);
+	$(this).find( '[name]' ).each( function(){
+           let value=$(this).val();
+           formData.append($(this).attr('name'),value);
+        });
+        $.ajax({
+           type: "POST",
+           url: "/updateUserAlert",
+           data:formData,
+	   processData:false,
+	   contentType:false,
+           success: function (data) {
+              window.location.reload();
+           },
+        }); 
     });
-    $parent.on('click', '.update', function (e) {
-	setMinDate();
-        e.preventDefault();
-        e.stopPropagation();
-	let pos = $('.update').index($(this));
-        let form = $('#update' + pos);
-	if($(this).html()=="Update"){
-	        form.css('display','block');
-		$(this).html("Don't Update");}
-	else{
-		form.css('display','none');
-		$(this).html('Update');} 
-                $parent.on('click', '#updateButton'+pos, function (e2) {
-		    e2.preventDefault();
-	     	    var dict="";
-		    let form=$(this).closest('form');
-	     	    let taskid = searchTable["items"][pos]["alert_id"];	
-	     	    dict+=taskid+',';
-		    form.find( '[name]' ).each( function(){
-		       let value=$(this).val();
-		       dict+=value+',';});
-             	$.ajax({
-         	   type: "POST",
-         	   url: "/updateGroupAlert",
-         	   data:{	
-		     value:dict,
-		   },
-            	   success: function (data) {
-                       console.log("AJAX RETURN: ", data);
-            	   },
-                });
-            }); 	
-    });
-});
-
-
+}); 
